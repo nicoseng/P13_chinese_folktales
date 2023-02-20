a7ebb14e02f75828a70f902cc6987b3b6a6a43d4
nicoseng <nicolas.sengmany@gmail.com>
2023-02-20 01:37:07 +0100

    mise à jour

--- chinese_folktales_website/story_importer.py
+++ chinese_folktales_website/story_importer.py
@@ -1,5 +1,9 @@
 from chinese_folktales_website.models import Story, Level
-from django.core.exceptions import ObjectDoesNotExist
+from os import path
+from bs4 import BeautifulSoup
+from slugify import slugify
+import markdown
+
 
 class StoryImporter:
     """
@@ -8,18 +12,35 @@ class StoryImporter:
 
     @staticmethod
     def load_story_list():
+
+        # open a file
+        file_path = path.relpath("/Users/nicolassengmany/Desktop/OCR/Python/Projets/P13/chinese_folktales/chinese_folktales_website/stories/兩隻小兔/兩隻小兔.md")
+        with open(file_path) as file1:
+            # read the file
+            read_content = file1.read()
+            print(read_content)
+
+        html = markdown.markdown(read_content)
+        print(html)
+
+        s = BeautifulSoup(html, 'html.parser')
+        for title in s.find_all("h1"):
+            section = s.new_tag("section")
+            section.string = title.string
+            section.attrs['id'] = slugify(title.string)
+            title.replace_with(section)
+
+        print(section)
+
         # Récupérer à partir de github (extraire les données ici)
         story_list = [
             {"title": "H1", "level": "Débutant"},
             {"title": "H2", "level": "Débutant+"},
             {"title": "H3", "level": "Intermédiaire"},
             {"title": "H4", "level": "Intermédiaire+"},
-            {"title": "H5", "level": "Intermédiaire+"},
+            {"title": "H5", "level": "Avancé"},
             {"title": "H6", "level": "Avancé+"},
-            {"title": "H7", "level": "Débutant+"},
-            {"title": "H8", "level": "Expert"},
-            {"title": "H9", "level": "Intermédiaire"},
-            {"title": "H10", "level": "Intermédiaire+"},
+            {"title": "H7", "level": "Expert"},
         ]
         return story_list
 
@@ -27,15 +48,11 @@ class StoryImporter:
     def inject_story_in_database(story_list, level_table, story_table):
 
         for element in story_list:
-            try:
-                story_data = Story.objects.get(title=element["title"])
-                print(story_data)
-            except ObjectDoesNotExist:
-                level_id = Level.objects.get(name=element["level"]).level_id
-                new_story_data = Story(
-                    level_id=Level.objects.get(level_id=level_id),
-                    title=element["title"])
-                new_story_data.save()
+            level_id = Level.objects.get(name=element["level"]).level_id
+            new_story_data = Story(
+                level_id=Level.objects.get(level_id=level_id),
+                title=element["title"])
+            new_story_data.save()
 
     @staticmethod
     def update_story_table(story_list, level_table, story_table):
@@ -62,56 +79,3 @@ class StoryImporter:
                     title=element["title"]
                 )
                 new_story_data.save()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # # 1. We extract each story from story_list
-        # story_checklist = []
-        # for element in story_list:
-        #     story_checklist.append(element["level"])
-        # print("story_checklist:", story_checklist)
-        #
-        # # 2. We extract each story from story_table
-        # story_table_checklist = []
-        # for story in story_table:
-        #     story_table_checklist.append(story.title)
-        #     if story.title not in story_checklist:
-        #         story_table.filter(title=story.title).delete()
-        # print("story_table:", story_table)
-        # print("story_table_checklist:", story_table_checklist)
-
-        # 3. We check if story from story_checklist
-        # and story from story_table_checklist are the same or not
-        # for element in story_list:
-        #     print(element["title"])
-        #     level_id = Level.objects.get(name=element["title"]).level_id
-        #     print(level_id)
-        #     if element["title"] not in story_table_checklist:
-        #         pass
-        #         # level_id = Level.objects.get(name=element["name"]).level_id
-        #         # print(level_id)
-        #         # story_data = Story(
-        #         #     level_id=Level.objects.get(name=element["name"]),
-        #         #     title=element["title"]
-        #         # )
-        #         # story_data.save()
-        #
-        # story_table = Story.objects.all()
-        # print("story_table:", story_table)
-        # return story_table
