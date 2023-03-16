6603fd163850d6f2a5ad7e621afa0a39b296d8fc
nicoseng <nicolas.sengmany@gmail.com>
2023-03-17 00:55:44 +0100

    mise à jour database dans settings.py

--- chinese_folktales_app/settings.py
+++ chinese_folktales_app/settings.py
@@ -79,33 +79,29 @@ WSGI_APPLICATION = 'chinese_folktales_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
+if os.environ.get("ENV", "development") == "production":
+    DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'chinese_folktales_bdd',
-            'USER': 'postgres',
-            'PASSWORD': 'a8ln17',
-            'HOST': '127.0.0.1',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ocr_chinese_folktales_bdd',
+            'USER': 'nsengmany',
+            'PASSWORD': 'lunaires',
+            'HOST': 'localhost',
             'PORT': '5432',
         }
     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'test_bdd',
-#             'USER': 'nsengmany',
-#             'PASSWORD': 'lunaires',
-#             'HOST': '127.0.0.1',
-#             'PORT': '5432',
-#         }
-#     }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
