e1402e9d469d56a9206b4e63c24a0a592cdee5ed
nicoseng <nicolas.sengmany@gmail.com>
2023-03-22 20:14:57 +0100

    mise à jour

--- chinese_folktales_app/settings.py
+++ chinese_folktales_app/settings.py
@@ -81,10 +81,10 @@ if os.environ.get("ENV", "development") == "production":
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'ocr_chinese_folktales_bdd',
-            'USER': '',
-            'PASSWORD': '',
+            'USER': 'nsengmany',
+            'PASSWORD': 'a8ln17',
             'HOST': '127.0.0.1',
-            'PORT': '5433',
+            'PORT': '5432',
         }
     }
 
